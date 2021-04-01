faf380657259a259f476f221dfe40baf7b548883
sidharth kaushik <sidharth.kaushik@atos.net>
2021-04-01 13:12:10 +0530

    ammo

--- AssaultCube.cpp
+++ AssaultCube.cpp
@@ -12,17 +12,34 @@ int main()
     DWORD procId = GetProcId(L"ac_client.exe");
 
     // GetModulebase address
+    uintptr_t moduleBase = GetModuleBaseAddress(procId, L"ac_client.exe");
 
     // Get Handle to process
+    HANDLE hProcess = 0;
+    hProcess = OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
 
     // Resolve base address of the pointer chain
+    uintptr_t dynamicPtrBaseAddr = moduleBase + 0x10F4F4;
+    std::cout << "DynamicPtrBaseAddr = " << "0x" << std::hex << dynamicPtrBaseAddr << std::endl;
 
     // Resolve our ammo pointer chain
+    std::vector<unsigned int> ammoOffsets = { 0x374, 0x14, 0x0 };
+    uintptr_t ammoAddr = FindDMAAddy(hProcess, dynamicPtrBaseAddr, ammoOffsets);
+    std::cout << "ammoAddr = " << "0x" << std::hex << ammoAddr << std::endl;
 
     // Read Ammo Value
+    int ammoValue = 0;
+    ReadProcessMemory(hProcess, (BYTE*)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
+    std::cout << "Current ammo = " << std::dec << ammoValue << std::endl;
 
     // Write to it
-
+    int newAmmo = 1337;
+    WriteProcessMemory(hProcess, (BYTE*)ammoAddr, &newAmmo, sizeof(newAmmo), nullptr);
+    
     // Read out again
-    std::cout << procId;
+    ReadProcessMemory(hProcess, (BYTE*)ammoAddr, &ammoValue, sizeof(ammoValue), nullptr);
+    std::cout << "Current ammo = " << std::dec << ammoValue << std::endl;
+
+    getchar();
+    return 0;
 }
